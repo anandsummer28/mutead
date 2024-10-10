22980d2d28a64143738dbf1560d121eafaf9f979
Anand <22359616+anandsummer28@users.noreply.github.com>
2024-10-11 01:58:56 +0530

    feat: added tab muter for youtube ads (#2)
    Co-authored-by: anandsummer28 <anandsummer28@users.noreply.github.com>

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
-// Check if the current site is "hotstar" or "jiocinema"
-// Check if the current site is "hotstar" or "jiocinema"
+// Check if the current site is "hotstar" or "jiocinema" or "youtube"
 const hostname = window.location.hostname;
 const isHotstar = hostname.includes('hotstar.com');
 const isJioCinema = hostname.includes('jiocinema.com');
+const isYoutube = hostname.includes('youtube.com');
 
-if (isHotstar || isJioCinema) {
+if (isHotstar || isJioCinema || isYoutube) {
   const observer = new MutationObserver((mutationsList, observer) => {
       mutationsList.forEach((mutation) => {
         if (mutation.type === 'childList') {
@@ -22,6 +22,13 @@ if (isHotstar || isJioCinema) {
                 console.log('Hotstar Tab muted!');
               });
             }
+
+            // for Youtube
+            if (isYoutube && node.nodeType === Node.ELEMENT_NODE && node.classList.contains('ytp-ad-player-overlay-layout')) {
+              chrome.runtime.sendMessage({ action: 'muteTabVolume' }, (response) => {
+                console.log('Youtube Tab muted!');
+              });
+            }
           });
 
           mutation.removedNodes.forEach((node) => {
@@ -33,7 +40,12 @@ if (isHotstar || isJioCinema) {
             // for hotstar
             if (isHotstar && node.getAttribute && node.getAttribute('data-testid') === 'ad-details') { 
               setTimeout(() => {chrome.runtime.sendMessage({action: 'restoreTabVolume'}, (response) => console.log('HotStar Tab unmuted!'))}, 0);
-          }
+            }
+
+             // for Youtube
+            if (isYoutube && node.nodeType === Node.ELEMENT_NODE && node.classList.contains('ytp-ad-player-overlay-layout')) {
+              setTimeout(() => {chrome.runtime.sendMessage({action: 'restoreTabVolume'}, (response) => console.log('Youtube Tab unmuted!'))}, 0);
+            }
           });
         }
       });
